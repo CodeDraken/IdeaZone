ce75e7413d1f8661db33aea211c4ee98bf70a676
Evan <evan4153@gmail.com>
2017-01-08 17:31:22 -0800

    Setup some folders, and package.json

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  return <h1>App</h1>
+}
