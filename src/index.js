6da7b494da947989d557675ef8822a3bc3136125
Evan <codedraken@gmail.com>
2017-01-14 04:38:07 +0000

    c9-pair Project Page HTML & CSS

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './css/main.css';
 
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
